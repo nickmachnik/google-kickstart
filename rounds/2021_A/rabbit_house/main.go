581ac66a3acbbc2f50bc0c89e47613acdb203277
Nick Noel Machnik <nick.machnik@bitpanda.com>
2021-05-02 18:36:56 +0200

    generator pattern in go first try

--- rounds/2021_A/rabbit_house/main.go
+++ rounds/2021_A/rabbit_house/main.go
@@ -1,15 +1,75 @@
 package main
 
 import (
-	"bufio"
+	"encoding/csv"
 	"fmt"
-	"os"
+	"io"
+	"strconv"
+	"strings"
 )
 
+type testCase struct {
+	rows int
+	cols int
+	grid [][]int
+	err  error
+}
+
+type testCaseOrErr struct {
+	testCase
+	err error
+}
+
 func main() {
-	reader := bufio.NewScanner(os.Stdin)
-	for reader.Scan() {
-		input := reader.Text()
-		fmt.Println(input)
+
+}
+
+// -------- Input reading -------- //
+
+// Go
+
+func NewTestCaseOrErr(rawLines []string) (res testCaseOrErr) {
+	strings.Split() rawLines[0]
+	strconv.ParseInt() 
+	return
+}
+
+func recordToPoint(record []string) (p Point, err error) {
+	if len(record) != 2 {
+		err = fmt.Errorf("Records must have two columns")
+		return
 	}
+	if p.X, err = strconv.ParseFloat(record[0], 64); err != nil {
+		return
+	}
+	if p.Y, err = strconv.ParseFloat(record[1], 64); err != nil {
+		return
+	}
+	return
+}
+
+// Go
+func LoadCsvDataToChannel(in io.Reader) <-chan PointOrErr {
+	out := make(chan PointOrErr)
+	go func() {
+		defer close(out)
+		reader := csv.NewReader(in)
+		for {
+			record, err := reader.Read()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				out <- PointOrErr{Err: err}
+				return
+			}
+			point, err := recordToPoint(record)
+			if err != nil {
+				out <- PointOrErr{Err: err}
+				return
+			}
+			out <- PointOrErr{Point: point}
+		}
+	}()
+	return out
 }
