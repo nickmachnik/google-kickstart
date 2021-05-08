00e85f1cbb571d099afda62c0a511374502ac1a8
Nick Noel Machnik <nick.machnik@bitpanda.com>
2021-05-08 22:05:58 +0200

    implement testCase parsers

--- rounds/2021_A/rabbit_house/main.go
+++ rounds/2021_A/rabbit_house/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"encoding/csv"
+	"bufio"
+	"fmt"
 	"io"
 	"strconv"
 	"strings"
@@ -24,7 +25,7 @@ func main() {
 
 // -------- Input reading -------- //
 
-func NewTestCase(rows, cols int, heights [][]int) testCase {
+func newTestCase(rows, cols int, heights [][]int) testCase {
 	return testCase{
 		rows,
 		cols,
@@ -32,9 +33,9 @@ func NewTestCase(rows, cols int, heights [][]int) testCase {
 	}
 }
 
-func NewTestCaseOrErr(rows, cols int, heights [][]int, err error) testCaseOrErr {
+func newTestCaseOrErr(rows, cols int, grid [][]int, err error) testCaseOrErr {
 	return testCaseOrErr{
-		NewTestCase(rows, cols, heights),
+		newTestCase(rows, cols, grid),
 		err,
 	}
 }
@@ -50,22 +51,90 @@ func parseIntFields(line string) (ints []int, err error) {
 	return
 }
 
-// Go
-func LoadCsvDataToChannel(in io.Reader) <-chan testCaseOrErr {
-	out := make(chan PointOrErr)
+func parseRowAndColNum(reader bufio.Reader) (row, col int, err error) {
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+	
+	intFields, err := parseIntFields(line)
+	if err != nil {
+		return
+	}
+
+	if len(intFields) != 2 {
+		err = fmt.Errorf("number of int fields in first line of test case not equal to 2")
+		return
+	}
+
+	row = intFields[0]
+	col = intFields[1]
+	
+	return
+}
+
+func parseGrid(rows int, cols int, reader bufio.Reader) ([][]int, error) {
+	var grid [rows][cols]int
+	
+	for i := 0; i < rows; i++ {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return grid, err
+		}
+	
+		row, err := parseIntFields(line)
+		if err != nil {
+			return grid, err
+		}
+	
+		grid[i] = row
+	}
+
+	return grid, nil
+}
+
+func loadTestCasesToChannel(reader bufio.Reader) <-chan testCaseOrErr {
+	out := make(chan testCaseOrErr)
+	
 	go func() {
 		defer close(out)
-		reader := csv.NewReader(in)
+		
+		numberOfTestCases, err := reader.ReadString('\n')
+		if err != nil {
+			out <- testCaseOrErr{err: err}
+			
+			return
+		}
+
+		for i := 0; i < numberOfTestCases; i++ {
+			rows, cols, err := parseRowAndColNum(reader)
+			if err != nil {
+				out <- testCaseOrErr{err: err}
+				
+				return
+			}
+
+			grid, err := parseGrid(rows, cols, reader)
+			if err != nil {
+				out <- testCaseOrErr{err: err}
+				
+				return
+			}
+
+			out <- newTestCaseOrErr(rows, cols, grid)
+		}
+
 		for {
-			record, err := reader.Read()
+			line, err := reader.ReadString('\n')
 			if err == io.EOF {
 				return
 			}
 			if err != nil {
-				out <- PointOrErr{Err: err}
+				out <- testCaseOrErr{err: err}
 				return
 			}
-			point, err := recordToPoint(record)
+
+			, err := recordToPoint(record)
 			if err != nil {
 				out <- PointOrErr{Err: err}
 				return
@@ -73,5 +142,6 @@ func LoadCsvDataToChannel(in io.Reader) <-chan testCaseOrErr {
 			out <- PointOrErr{Point: point}
 		}
 	}()
+
 	return out
 }
